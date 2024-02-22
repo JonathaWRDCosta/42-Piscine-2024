2ed44329b46f6cabd3fb84712cb7b01edd5405ea
Jonatha Costa <jonathawiliamsrdcosta@gmail.com>
2024-02-22 10:34:09 +0000

    C00 Improvement/.gitignore changes

--- C-00/ex06/ft_print_comb2.c
+++ C-00/ex06/ft_print_comb2.c
@@ -1,52 +1,57 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jonatha <jonatha@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/22 05:26:11 by jonatha           #+#    #+#             */
-/*   Updated: 2024/02/22 10:20:20 by jonatha          ###   ########.fr       */
+/*   Created: 2024/02/22 07:20:39 by jonatha           #+#    #+#             */
+/*   Updated: 2024/02/22 10:28:40 by jonatha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void ft_print_comb(void)
+void ft_putchar(char c)
+{
+  write(1, &c, 1);
+}
+
+void ft_print_comb2(void)
 {
   int a;
   int b;
-  int c;
 
-  a = '0';
-  while(a <= '7')
+  a = 00;
+  while (a <= 98)
   {
     b = a + 1;
-    while (b <= '8')
+    while (b <= 99)
     {
-      c = b + 1;
-      while (c <= '9')
+      ft_putchar(a / 10 + '0');
+      ft_putchar(a % 10 + '0');
+      ft_putchar(' ');
+      ft_putchar(b / 10 + '0');
+      ft_putchar(b % 10 + '0');
+      if (a != 98 || b != 99)
       {
-        write(1, &a, 1);
-        write(1, &b, 1);
-        write(1, &c, 1);
-        if (a != '7' || b != '8' || c != '9')
-        {
-          write(1, ",", 1);
-          write(1, " ", 1);
-        }
-      c++;
+      ft_putchar(',');
+      ft_putchar(' ');
       }
+
       b++;
     }
     a++;
   }
+  
+
+  
 }
 
 /*
 int main(void)
 {
-  ft_print_comb();
+  ft_print_comb2();
   return(0);
 }
 */
